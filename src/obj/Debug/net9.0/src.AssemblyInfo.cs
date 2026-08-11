8cec3aad9c8a845f1e3dbcf70d93ca26b3b6d386
Andersson Beltre <20210853@itla.edu.do>
2026-08-11 17:54:18 -0400

    Actualizando Dockerfile a .NET 9.0

--- src/obj/Debug/net9.0/src.AssemblyInfo.cs
+++ src/obj/Debug/net9.0/src.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("src")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+148b9b0abde38981d9abf146c89f746e043228f6")]
 [assembly: System.Reflection.AssemblyProductAttribute("src")]
 [assembly: System.Reflection.AssemblyTitleAttribute("src")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
